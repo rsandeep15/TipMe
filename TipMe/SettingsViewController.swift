aa3f93eaddee4b658a2039143ab8f2958411311e
Sandeep Raghunandhan <saraghun@ucsd.edu>
2016-12-15 18:47:59 -0800

    Working on README

--- TipMe/SettingsViewController.swift
+++ TipMe/SettingsViewController.swift
@@ -28,16 +28,4 @@ class SettingsViewController: UIViewController {
         defaults.set(segValue.selectedSegmentIndex, forKey: "index")
         defaults.synchronize()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
